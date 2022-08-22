7b6c502638799580481b8edfc92e3064ce7a7045
Khan M. Tabish <khantabish@ttnpl-2026.local>
2022-08-22 16:52:16 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Files Copy
+
+This repo is used to copy the content of one folder to another. It will react the filder name from the arguments we passed. 
+
+
+
+```bash
+  npm install   
+  node run customer
+```
--- run.js
+++ run.js
@@ -1,7 +1,13 @@
 const fs = require("fs-extra");
-let sourceDir = "./assets/customer1";
+var arguments = process.argv;
+let sourceDir = `./assets/${arguments[2]}`;
 let destDir = "./dist";
 try {
+  fs.emptyDirSync(destDir);
+  console.log("Deleted the files from the dis Folder!");
+  console.log(
+    `copying the content from the ${arguments[2]} to the dist folder`
+  );
   fs.copySync(sourceDir, destDir);
   console.log("Assets Copies Successfully!");
 } catch (err) {
